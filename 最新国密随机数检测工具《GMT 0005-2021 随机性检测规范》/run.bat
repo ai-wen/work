b90065624ded1ed6bd7359f20bd683561945fd51
liukanglu <liukanglu@longmai.com.cn>
2022-11-01 16:00:59 +0800

    go

--- 最新国密随机数检测工具《GMT 0005-2021 随机性检测规范》/run.bat
+++ 最新国密随机数检测工具《GMT 0005-2021 随机性检测规范》/run.bat
@@ -11,5 +11,18 @@ cd ..
 
 rdgen\rdgen.exe -s 1000 -n 1000000 -o data
 
+:::rddetector\rddetector.exe -i data -o RandomnessTestReport.csv
 
-rddetector\rddetector.exe -i data -o RandomnessTestReport.csv
+cd dll
+
+go build -buildmode=c-shared -o random.dll randomTest.go
+
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
+
+lib.exe /def:random.def /machine:x64 /out:random64.lib
+
+cl.exe main.cpp
+
+cd ..
+
+dll\main.exe data >CTestReport.csv

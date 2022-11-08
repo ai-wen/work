eb0b8a07252809bd754877e006ddafa8249550ef
ai-wen <363042868@qq.com>
2022-11-08 18:16:49 +0800

    go

--- 最新国密随机数检测工具《GMT 0005-2021 随机性检测规范》/ui/RandomCheckTool.go
+++ 最新国密随机数检测工具《GMT 0005-2021 随机性检测规范》/ui/RandomCheckTool.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/fs"
 	"io/ioutil"
 	"math"
 	"os"
@@ -15,20 +14,20 @@ import (
 	"sync/atomic"
 	"time"
 
+	"golang.org/x/text/encoding/simplifiedchinese"
+
 	"github.com/Trisia/randomness"
 	"github.com/andlabs/ui"
 	_ "github.com/andlabs/ui/winmanifest"
 )
 
-//go get github.com/andlabs/libui
-//go get github.com/andlabs/ui
-
 var mainwin *ui.Window
 
-var itemStr = [27]string{"单比特频数检测", "块内频数检测 m=10000", "扑克检测 m=4", "扑克检测 m=8", "重叠子序列检测 m=3 P1", "重叠子序列检测 m=3 P2", "重叠子序列检测 m=5 P1", "重叠子序列检测 m=5 P2", "游程总数检测", "游程分布检测", "块内最大1游程检测 m=10000","块内最大0游程检测 m=10000",  "二元推导检测 k=3", "二元推导检测 k=7", "自相关检测 d=1", "自相关检测 d=2", "自相关检测 d=8", "自相关检测 d=16", "矩阵秩检测", "累加和前向检测","累加和后向检测", "近似熵检测 m=2", "近似熵检测 m=5", "线性复杂度检测 m=500", "线性复杂度检测 m=1000", "Maurer通用统计检测 L=7 Q=1280", "离散傅里叶检测"}
+var itemStr = [27]string{"单比特频数检测", "块内频数检测 m=10000", "扑克检测 m=4", "扑克检测 m=8", "重叠子序列检测 m=3 P1", "重叠子序列检测 m=3 P2", "重叠子序列检测 m=5 P1", "重叠子序列检测 m=5 P2", "游程总数检测", "游程分布检测", "块内最大1游程检测 m=10000", "块内最大0游程检测 m=10000", "二元推导检测 k=3", "二元推导检测 k=7", "自相关检测 d=1", "自相关检测 d=2", "自相关检测 d=8", "自相关检测 d=16", "矩阵秩检测", "累加和前向检测", "累加和后向检测", "近似熵检测 m=2", "近似熵检测 m=5", "线性复杂度检测 m=500", "线性复杂度检测 m=1000", "Maurer通用统计检测 L=7 Q=1280", "离散傅里叶检测"}
 
 var selectArry [len(itemStr)]int
 var selectslice []int
+var cnt []int32
 
 var checkboxs [len(itemStr)]*ui.Checkbox
 var labels_succ [len(itemStr)]*ui.Label
@@ -43,6 +42,8 @@ var labeltime *ui.Label
 var labeltimebegin *ui.Label
 var labeltimeend *ui.Label
 var buttonOK *ui.Button
+var buttonFile *ui.Button
+
 var setcnt int
 
 // Alpha 显著性水平α
@@ -54,9 +55,14 @@ var entryAlpha *ui.Entry
 var entryPath *ui.Entry
 var entryOutPath *ui.Entry
 
+var reportPath string
+var inputPath string
+
 var prosessbar *ui.ProgressBar
 
 var seletctAll bool
+
+var running int32
 
 type R struct {
 	Name string
@@ -68,32 +74,32 @@ func worker(jobs <-chan string, out chan<- *R) {
 		buf, _ := os.ReadFile(filename)
 		bits := randomness.B2bitArr(buf)
 		buf = nil
-		arr := make([]float64, 0, len(selectslice))
 
+		arr := make([]float64, 0, len(selectslice))
 
 		if selectArry[0] == 1 {
 			//"单比特频数检测"
 			p, _ := randomness.MonoBitFrequencyTest(bits)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[1] == 1 {
 			//"块内频数检测 m=10000"
 			p, _ := randomness.FrequencyWithinBlockTest(bits)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[2] == 1 {
 			//"扑克检测 m=4"
 			//"扑克检测 m=8"
 			p, _ := randomness.PokerProto(bits, 4)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[3] == 1 {
 			p, _ := randomness.PokerProto(bits, 8)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[4] == 1 || selectArry[5] == 1 {
 			//"重叠子序列检测 m=3 P1"
@@ -120,40 +126,40 @@ func worker(jobs <-chan string, out chan<- *R) {
 		}
 
 		if selectArry[8] == 1 {
-			//"游程总数检测"			
+			//"游程总数检测"
 			p, _ := randomness.RunsTest(bits)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[9] == 1 {
-			//"游程分布检测"			
+			//"游程分布检测"
 			p, _ := randomness.RunsDistributionTest(bits)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[10] == 1 {
 			//"块内最大1游程检测 m=10000"
 			p, _ := randomness.LongestRunOfOnesInABlockTest(bits, true)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[11] == 1 {
 			//"块内最大0游程检测 m=10000"
 			p, _ := randomness.LongestRunOfOnesInABlockTest(bits, false)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[12] == 1 {
-			//"二元推导检测 k=3"			
+			//"二元推导检测 k=3"
 			p, _ := randomness.BinaryDerivativeProto(bits, 3)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[13] == 1 {
 			//"二元推导检测 k=7"
 			p, _ := randomness.BinaryDerivativeProto(bits, 7)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[14] == 1 {
 			//"自相关检测 d=1"
@@ -162,69 +168,69 @@ func worker(jobs <-chan string, out chan<- *R) {
 			//"自相关检测 d=16"
 			p, _ := randomness.AutocorrelationProto(bits, 1)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[15] == 1 {
 			p, _ := randomness.AutocorrelationProto(bits, 2)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[16] == 1 {
 			p, _ := randomness.AutocorrelationProto(bits, 8)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[17] == 1 {
 			p, _ := randomness.AutocorrelationProto(bits, 16)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[18] == 1 {
 			//"矩阵秩检测"
 			p, _ := randomness.MatrixRankTest(bits)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[19] == 1 {
 			//"累加和检测"  前向
 			p, _ := randomness.CumulativeTest(bits, true)
 			arr = append(arr, p)
-		}		
+		}
 		if selectArry[20] == 1 {
 			//"累加和检测"  后向
 			p, _ := randomness.CumulativeTest(bits, false)
 			arr = append(arr, p)
-		}	
+		}
 
 		if selectArry[21] == 1 {
 			//"近似熵检测 m=2"
 			//"近似熵检测 m=5"
 			p, _ := randomness.ApproximateEntropyProto(bits, 2)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[22] == 1 {
 			p, _ := randomness.ApproximateEntropyProto(bits, 5)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[23] == 1 {
 			//"线性复杂度检测 m=500"
 			//"线性复杂度检测 m=1000"
 			p, _ := randomness.LinearComplexityProto(bits, 500)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[24] == 1 {
 			p, _ := randomness.LinearComplexityProto(bits, 1000)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[25] == 1 {
 			//"Maurer通用统计检测 L=7 Q=1280"
 			p, _ := randomness.MaurerUniversalTest(bits)
 			arr = append(arr, p)
-		}		
+		}
 
 		if selectArry[26] == 1 {
 			//"离散傅里叶检测"
@@ -238,8 +244,6 @@ func worker(jobs <-chan string, out chan<- *R) {
 	}
 }
 
-//var lock sync.Mutex
-
 // 结果集写入文件工作器
 func resultWriter(in <-chan *R, w io.StringWriter, cnt []int32, wg *sync.WaitGroup) {
 	for r := range in {
@@ -250,20 +254,35 @@ func resultWriter(in <-chan *R, w io.StringWriter, cnt []int32, wg *sync.WaitGro
 		for j := 0; j < len(r.P); j++ {
 			if r.P[j] >= 0.01 {
 				atomic.AddInt32(&cnt[j], 1)
-				//崩溃
-				//labels_succ[selectslice[j]].SetText(fmt.Sprintf("%d", cnt[j]))
 			} else {
 				atomic.AddInt32(&failcnt[j], 1)
-				//labels_fail[selectslice[j]].SetText(fmt.Sprintf("%d", failcnt[j]))
 			}
 			_, _ = w.WriteString(fmt.Sprintf(", %0.6f", r.P[j]))
 		}
-		//lock.Unlock()
 		_, _ = w.WriteString("\n")
 
 		wg.Done()
 	}
 }
+
+func StrToGBK(str string) string {
+	sysType := runtime.GOOS
+	if sysType == "windows" {
+		//将utf-8编码的字符串转换为GBK编码
+		ret, _ := simplifiedchinese.GBK.NewEncoder().String(str)
+		return ret //如果转换失败返回空字符串
+	} else {
+		return str
+	}
+
+	//如果是[]byte格式的字符串，可以使用Bytes方法
+	//b, err := simplifiedchinese.GBK.NewEncoder().Bytes([]byte(str))
+	//return string(b)
+}
+
+var startTime time.Time
+var processfile int32
+var percent int32
 
 func makeBasicControlsPage() ui.Control {
 
@@ -395,22 +414,14 @@ func makeBasicControlsPage() ui.Control {
 
 	entryOutPath := ui.NewEntry()
 
-	button := ui.NewButton("选择样本文件")
+	buttonFile = ui.NewButton("选择样本文件")
 	entryPath = ui.NewEntry()
 	entryPath.SetReadOnly(true)
-	button.OnClicked(func(*ui.Button) {
+	buttonFile.OnClicked(func(*ui.Button) {
 		setcnt = 0
 
 		filename := ui.OpenFile(mainwin)
 		if filename != "" {
-			/*
-				filepath.Walk(filepath.Dir(filename), func(p string, _ fs.FileInfo, _ error) error {
-					if strings.HasSuffix(p, ".bin") || strings.HasSuffix(p, ".dat") {
-						setcnt++
-					}
-					return nil
-				})
-			*/
 			files, err := ioutil.ReadDir(filepath.Dir(filename))
 			if err == nil {
 				for _, file := range files {
@@ -418,13 +429,12 @@ func makeBasicControlsPage() ui.Control {
 					if strings.HasSuffix(file.Name(), ".bin") || strings.HasSuffix(file.Name(), ".dat") {
 						setcnt++
 					}
-					//fmt.Println(file.Name())
 				}
 			}
 		}
 
 		if setcnt == 0 {
-			ui.MsgBoxError(mainwin, "国密随机数检测工具", "样本文件必须是 .dat 或 .bin文件")
+			ui.MsgBoxError(mainwin, "随机数检测工具", "样本文件必须是 .dat 或 .bin文件")
 			labelsetAlphaNum.SetText("")
 			labelsetNum.SetText("")
 		} else {
@@ -433,7 +443,12 @@ func makeBasicControlsPage() ui.Control {
 			buf, _ := os.ReadFile(filename)
 			labelsetbit.SetText(fmt.Sprintf("%dbit %dbyte", len(buf)*8, len(buf)))
 
-			outpath := path.Join(filepath.Dir(filename), "/RandomnessTestReport.csv")
+			if len(buf) < (7 * 1280) {
+				checkboxs[25].SetChecked(false)
+				ui.MsgBoxError(mainwin, "随机数检测工具", fmt.Sprintf("Maurer通用统计检测 数据长度至少要满足 L*Q %dbyte", 7*1280))
+			}
+
+			outpath := path.Join(filepath.Dir(filepath.Dir(filename)), "/RandomnessTestReport.csv")
 			//outpath := filepath.Dir(filename) + "/RandomnessTestReport.csv"
 			_ = os.MkdirAll(filepath.Dir(outpath), os.FileMode(0600))
 
@@ -446,7 +461,7 @@ func makeBasicControlsPage() ui.Control {
 	})
 	hboxpath.Append(ui.NewLabel("样本路径:"), false)
 	hboxpath.Append(entryPath, true)
-	hboxpath.Append(button, false)
+	hboxpath.Append(buttonFile, false)
 
 	//vbox.Append(hboxpath, false)
 	vboxOp.Append(hboxpath, false)
@@ -459,10 +474,21 @@ func makeBasicControlsPage() ui.Control {
 	buttonOK.OnClicked(func(*ui.Button) {
 
 		filename := entryOutPath.Text()
+
 		if filename != "" {
 
 			buttonOK.Disable()
-			button.Disable()
+			buttonFile.Disable()
+
+			for j := 0; j < len(itemStr); j++ {
+				labels_succ[j].SetText("0")
+				labels_fail[j].SetText("0")
+			}
+			prosessbar.SetValue(0)
+			labeltime.SetText("")
+			labeltimeend.SetText("")
+			labeltimebegin.SetText("")
+
 			_ = os.MkdirAll(filepath.Dir(filename), os.FileMode(0600))
 
 			selectslice = selectslice[0:0]
@@ -478,34 +504,26 @@ func makeBasicControlsPage() ui.Control {
 				failcnt[i] = 0
 			}
 
+			inputPath = entryPath.Text()
+
+			inputPath = filepath.Dir(inputPath)
+			reportPath = entryOutPath.Text()
+
+			startTime = time.Now()
+			labeltimebegin.SetText(startTime.Format("2006.01.02 15:04:05"))
+
 			//线程
 			go func() {
-
-				startTime := time.Now()
-				startTimestr := startTime.Format("2006.01.02 15:04:05")
-				labeltimebegin.SetText(startTimestr)
 
 				n := runtime.NumCPU()
 				out := make(chan *R)
 				jobs := make(chan string)
 
-				inputPath := entryPath.Text()
-				if filename == "" {
-					fmt.Printf(">> 请选择样本文件")
-					ui.MsgBoxError(mainwin, "国密随机数检测工具", "请选择样本文件 ")
-					buttonOK.Enable()
-					button.Enable()
-					return
-				}
-
-				inputPath = filepath.Dir(inputPath)
-				reportPath := entryOutPath.Text()
 				w, err := os.OpenFile(reportPath, os.O_RDWR|os.O_TRUNC|os.O_CREATE, os.FileMode(0600))
 				if err != nil {
 					fmt.Printf(">> 无法打开写入报告文件")
-					ui.MsgBoxError(mainwin, "国密随机数检测工具", "无法打开写入文件 "+reportPath)
-					buttonOK.Enable()
-					button.Enable()
+					//ui.MsgBoxError(mainwin, "随机数检测工具", "无法打开写入文件 "+reportPath)
+					running = 0
 					return
 				}
 				defer w.Close()
@@ -518,10 +536,28 @@ func makeBasicControlsPage() ui.Control {
 					reportheader += ","
 				}
 				reportheader += "\n"
+
+				reportheader = StrToGBK(reportheader)
 				_, _ = w.WriteString(reportheader)
 
+				processfile = 0
+				if setcnt > 100 {
+					percent = int32(setcnt / 100)
+				} else {
+					percent = int32(100 / setcnt)
+				}
+
+				files, err := ioutil.ReadDir(inputPath)
+				if err != nil {
+					//ui.MsgBoxError(mainwin, "随机数检测工具", "打开写入文件失败")
+					running = 0
+					return
+				}
+
 				var wg sync.WaitGroup
-				var cnt = make([]int32, len(selectslice))
+				cnt = make([]int32, len(selectslice))
+
+				running = 1
 
 				wg.Add(setcnt)
 
@@ -532,78 +568,32 @@ func makeBasicControlsPage() ui.Control {
 					go worker(jobs, out)
 				}
 
-				process := 0
-				var percent int
-				if setcnt > 100 {
-					percent = (setcnt / 100)
-				} else {
-					percent = (100 / setcnt)
-				}
-
 				// 结果工作器
-				go filepath.Walk(inputPath, func(p string, _ fs.FileInfo, _ error) error {
-					if strings.HasSuffix(p, ".bin") || strings.HasSuffix(p, ".dat") {
-						process++
-						//fmt.Printf("%d %d \n", process, percent)
-						if setcnt > 100 {
-							prosessbar.SetValue(process / percent)
-						} else {
-							prosessbar.SetValue(process * percent)
-						}
+				for _, file := range files {
+					if strings.HasSuffix(file.Name(), ".bin") || strings.HasSuffix(file.Name(), ".dat") {
 
-						for j := 0; j < len(selectslice); j++ {
-							labels_succ[selectslice[j]].SetText(fmt.Sprintf("%d", cnt[j]))
-							labels_fail[selectslice[j]].SetText(fmt.Sprintf("%d", failcnt[j]))
-						}
+						atomic.AddInt32(&processfile, 1)
 
-						labeltime.SetText(time.Now().Format("2006.01.02 15:04:05"))
-						jobs <- p
+						jobs <- (path.Join(inputPath, "/", file.Name()))
 					}
-					return nil
-				})
+				}
 
 				wg.Wait()
 
-				_, _ = w.WriteString("总计")
+				running = 0
+
+				reportheader = "总计"
+				reportheader = StrToGBK(reportheader)
+				_, _ = w.WriteString(reportheader)
 				for i := 0; i < len(cnt); i++ {
 					_, _ = w.WriteString(fmt.Sprintf(", %d", cnt[i]))
 				}
 				_, _ = w.WriteString("\n")
 
-				prosessbar.SetValue(100)
-
-				elapsedTime := time.Since(startTime) / time.Second // duration in s
-				labeltime.SetText(fmt.Sprintf("%d 分钟 %d 秒", elapsedTime/60, elapsedTime%60))
-				labeltimeend.SetText(time.Now().Format("2006.01.02 15:04:05"))
-
-				for j := 0; j < len(selectslice); j++ {
-					labels_succ[selectslice[j]].SetText(fmt.Sprintf("%d", cnt[j]))
-					labels_fail[selectslice[j]].SetText(fmt.Sprintf("%d", failcnt[j]))
-				}
-
-				var errStr string
-				var numCount int = 0
-				//通过的组数
-				var numpass int32 = int32(math.Ceil((1 - Alpha - 3*math.Sqrt(float64((Alpha*(1-Alpha))/float64(setcnt)))) * float64(setcnt)))
-				for j := 0; j < len(selectslice); j++ {
-
-					//num, _ := strconv.Atoi(labels_succ[selectslice[j]].Text())
-					if cnt[j] < numpass {
-						numCount++
-						errStr += itemStr[selectslice[j]]
-						errStr += ",\n"
-					}
-				}
-
-				if 0 == numCount {
-					ui.MsgBox(mainwin, "成功", "所有检测项全部通过")
-				} else {
-					ui.MsgBoxError(mainwin, "失败", fmt.Sprintf("有%d项检测未通过:%s\n", numCount, errStr))
-				}
-
-				buttonOK.Enable()
-				button.Enable()
 			}()
+		} else {
+			ui.MsgBoxError(mainwin, "随机数检测工具", "请选择样本文件 ")
+			return
 		}
 	})
 
@@ -645,7 +635,7 @@ func makeBasicControlsPage() ui.Control {
 		0, 1, 1, 1,
 		true, ui.AlignCenter, false, ui.AlignCenter)
 
-	grid.Append(ui.NewLabel("北京世纪龙脉科技有限公司 V1.2"),
+	grid.Append(ui.NewLabel("北京世纪龙脉科技有限公司 V1.4"),
 		0, 2, 1, 1,
 		true, ui.AlignCenter, true, ui.AlignCenter)
 
@@ -656,7 +646,7 @@ func makeBasicControlsPage() ui.Control {
 }
 
 func setupUI() {
-	mainwin = ui.NewWindow("国密随机数检测工具-北京世纪龙脉科技有限公司", 640, 480, true)
+	mainwin = ui.NewWindow("随机数检测工具-北京世纪龙脉科技有限公司", 640, 480, true)
 	mainwin.OnClosing(func(*ui.Window) bool {
 		ui.Quit()
 		return true
@@ -673,13 +663,100 @@ func setupUI() {
 	tab.Append("GMT 0005-2021 随机性检测", makeBasicControlsPage())
 	tab.SetMargined(0, true)
 
-	//labels_succ[0].SetText("1")
 	mainwin.Show()
+	go counter()
 }
 
+func counter() {
+
+	for {
+		time.Sleep(1 * time.Second)
+
+		ui.QueueMain(func() {
+
+			if 1 == running {
+
+				if setcnt > 100 {
+					prosessbar.SetValue(int(processfile / percent))
+				} else {
+					prosessbar.SetValue(int(processfile * percent))
+				}
+
+				labeltimeend.SetText(time.Now().Format("2006.01.02 15:04:05"))
+				for j := 0; j < len(selectslice); j++ {
+					labels_succ[selectslice[j]].SetText(fmt.Sprintf("%d", cnt[j]))
+					labels_fail[selectslice[j]].SetText(fmt.Sprintf("%d", failcnt[j]))
+				}
+			} else if 0 == running {
+
+				prosessbar.SetValue(100)
+
+				elapsedTime := time.Since(startTime) / time.Second // duration in s
+				labeltime.SetText(fmt.Sprintf("%d 分钟 %d 秒", elapsedTime/60, elapsedTime%60))
+				labeltimeend.SetText(time.Now().Format("2006.01.02 15:04:05"))
+
+				buttonOK.Enable()
+				buttonFile.Enable()
+
+				for j := 0; j < len(selectslice); j++ {
+					labels_succ[selectslice[j]].SetText(fmt.Sprintf("%d", cnt[j]))
+					labels_fail[selectslice[j]].SetText(fmt.Sprintf("%d", failcnt[j]))
+				}
+
+				var errStr string
+				var numCount int = 0
+				//通过的组数
+				var numpass int32 = int32(math.Ceil((1 - Alpha - 3*math.Sqrt(float64((Alpha*(1-Alpha))/float64(setcnt)))) * float64(setcnt)))
+				for j := 0; j < len(selectslice); j++ {
+
+					if cnt[j] < numpass {
+						numCount++
+						errStr += itemStr[selectslice[j]]
+						errStr += ",\n"
+					}
+				}
+
+				running = 3
+
+				if 0 == numCount {
+					ui.MsgBox(mainwin, "成功", "所有检测项全部通过")
+				} else {
+					ui.MsgBoxError(mainwin, "失败", fmt.Sprintf("有%d项检测未通过:%s\n", numCount, errStr))
+				}
+
+				processfile = 0
+				percent = 1
+
+			} else {
+
+			}
+		})
+	}
+}
 func main() {
+	running = 3
+	percent = 1
 	ui.Main(setupUI)
 }
+
+//go 大于1.8
+//Debian、Ubuntu 等：sudo apt-get install libgtk-3-dev
+//Red Hat / Fedora 等：sudo dnf install gtk3-devel
+//go get github.com/andlabs/libui
+//go get github.com/andlabs/ui
+//go get golang.org/x/text
+//go get github.com/Trisia/randomness
+//go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+
+//apt-get install golang-go
+//wget https://dl.google.com/go/go1.19.3.linux-amd64.tar.gz
+//tar -zxvf go1.19.3.linux-amd64.tar.gz
+//解压到/home/lm/Desktop/
+//export GOROOT=/home/lm/Desktop/go
+//export GOPATH=/home/lm/go
+//export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+//go version
+//go mod init RandomCheckTool
 
 //隐藏命令行
 //go build -ldflags="-H windowsgui"  RandomCheckTool.go
@@ -689,4 +766,8 @@ func main() {
 //main.rc
 //IDI_ICON1 ICON "logo.ico"
 //windres -o main.syso main.rc
-//go build -ldflags="-H windowsgui"
+//windres -i rc/main.rc -O coff -o main.syso
+//go build -o RandomCheckTool.exe  -ldflags="-H windowsgui"
+
+//其它
+//go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
